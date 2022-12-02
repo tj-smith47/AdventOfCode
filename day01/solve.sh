420c34a1d34c09cc214a3d25a5565854297394b5
TJ Smith <tj.smith@salesloft.com>
2022-12-02 08:51:41 -0800

    Initial commit.

---
+++ day01/solve.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+declare -i run=0
+declare -i sum=0
+
+sed -e 's/^$/asdf/g' inputs > input
+
+while read line; do
+    case $line in
+        "asdf" | asdf | $test)
+            echo $sum >> sums && sum=0
+            ;;
+        *)
+            sum+=$line
+            ;;
+    esac
+done < input
+
+grep -Eo '[0-9]+' sums | sort -n
